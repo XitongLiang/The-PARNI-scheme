6a7dc61fa87bd5fdf233fb26dd71d77ab63dfa18
Xitong Liang <47273639+XitongLiang@users.noreply.github.com>
2021-11-01 13:02:46 +0000

    Delete Samplers directory

--- Samplers/other supportive functions/Compute_Delta.R
+++
@@ -1,3 +0,0 @@
-compute_Delta <- function(PIPs) {
-  2 * (sum(PIPs[PIPs >= 0.5]) + sum(1 - PIPs[PIPs < 0.5]))
-}
--- Samplers/other supportive functions/update_temp_in_LA.R
+++
@@ -1,4 +0,0 @@
-update_temp_in_LA <- function(LA, new_temp) {
-  LA$log_post <- new_temp*LA$llh + LA$lmp
-  return(LA)
-}
