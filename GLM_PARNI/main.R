a4bc97a12b65193725f77e51e5bbf7645654d374
Xitong Liang <47273639+XitongLiang@users.noreply.github.com>
2023-08-17 17:52:47 +0100

    Add files via upload

---
+++ GLM_PARNI/main.R
@@ -0,0 +1,97 @@
+setwd("/Users/zejingshao/Desktop/BVS for GLM/codes")
+
+
+source('Version7/GLM_ADS.R')
+source('Version7/GLM_PARNI.R')
+
+source('Version7/PARNI_proposal.R')
+source("Version7/make_fixed_variances.R")
+
+source("Version7/logit_e.R")
+source("Version7/logit.R")
+source("Version7/make_fixed_variances.R")
+
+source("Version7/sample_ind.R")
+
+
+source("Version7/mvnorm_trans.R")
+source("Version7/bounded_x.R")
+
+
+# random g
+source("Version7/log_half_cauchy_wj.R")
+source("Version7/rhcauchy.R")
+source("Version7/hc_update_g.R")
+
+
+
+
+
+
+# linear
+
+
+
+
+
+
+
+# logistic
+source("Version7/logistic_log_post_beta.R")
+source("Version7/logistic_ALA2.R")
+source("Version7/logistic_ALA.R")
+source("Version7/logistic_ALA_rb.R")
+
+
+# data augmentation
+source("Version7/rpolyagamma.R")
+source("Version7/logistic_update_matrices.R")
+source("Version7/logistic_update_omega.R")
+source("Version7/logistic_DA_rb.R")
+
+# LA/CPM
+source("Version7/logistic_IRLS.R")
+source("Version7/logistic_log_post_beta.R")
+source("Version7/logistic_laplace.R")
+source("Version7/logistic_cpm.R")
+
+
+
+
+# Cox
+source("Version7/cox_functions.R")
+source("Version7/cox_NR_coxph.R")
+source("Version7/cox_log_post_beta.R")
+
+source("Version7/cox_ALA_rb.R")
+
+source("Version7/cox_compute_ALA_W.R")
+
+source("Version7/cox_ALA2.R")
+source("Version7/cox_ALA.R")
+
+# LA/CPM
+source("Version7/cox_laplace.R")
+source("Version7/cox_cpm.R")
+
+
+
+
+# Weibull
+source("Version7/weibull_ALA_rb.R")
+source("Version7/weibull_ALA2.R")
+source("Version7/weibull_ALA.R")
+
+source("Version7/weibull_update_k.R")
+source("Version7/weibull_NR.R")
+source("Version7/weibull_log_post_beta.R")
+
+# LA/CPM
+source("Version7/weibull_cpm.R")
+source("Version7/weibull_laplace.R")
+
+
+
+
+
+
